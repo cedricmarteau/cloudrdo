ec892e4878ca324030506a3e12251812af74de0b
cedricmarteau <cedric.marteau@gmail.com>
2015-02-26 17:05:41 +0100

    player

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,24 @@
 var socket = io.connect('http://localhost');
+var client_id = "45543d60298a07d51ca66c31835dfa26",
+    api = "http://api.soundcloud.com";
+
+var currentSound = null;
+
+SC.initialize({
+  client_id: client_id
+});
+
+SC.stream("/tracks/293", function(sound){
+  currentSound = sound;
+});
+
+$("#play").on("click",function(){
+  if ($("#player").is(".playing")){
+    $("#player").removeClass('playing');
+    currentSound.pause();
+  }else{
+    $("#player").addClass('playing');
+    currentSound.play();
+  }
+
+});
